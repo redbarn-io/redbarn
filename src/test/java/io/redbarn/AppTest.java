6be4f2a74a3a8147195c497e66cbc8be4761ca5d
Mike Atkisson <michael.atkisson@redbarn.io>
2017-05-28 15:07:56 -0400

    Adds initial project files

---
+++ src/test/java/io/redbarn/AppTest.java
@@ -0,0 +1,19 @@
+package io.redbarn;
+
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
