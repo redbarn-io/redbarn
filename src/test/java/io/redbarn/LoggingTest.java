79f5a9c6613e59319a611bcdb87e4bb1c9cce667
Mike Atkisson <michael.atkisson@redbarn.io>
2017-05-28 15:50:00 -0400

    Adds the logging facade

---
+++ src/test/java/io/redbarn/LoggingTest.java
@@ -0,0 +1,15 @@
+package io.redbarn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoggingTest {
+
+    @Test
+    public void Logger_InstantiatedViaFacade_IsNotNull() {
+        Logger logger = LoggerFactory.getLogger(LoggingTest.class);
+        Assert.assertNotNull(logger);
+    }
+}
