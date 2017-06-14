91f308e3ed3e77e79b80b46026918038700db70d
Mike Atkisson <michael.atkisson@redbarn.io>
2017-06-14 11:56:29 -0400

    Exposes the lodash 'getParams' mixin

--- src/main/java/io/redbarn/LodashScriptImage.java
+++ src/main/java/io/redbarn/LodashScriptImage.java
@@ -1,8 +1,12 @@
 package io.redbarn;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
 import javax.script.ScriptEngine;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Used to invoke Lodash methods in Java.
@@ -17,8 +21,8 @@ public class LodashScriptImage extends AbstractScriptImage {
     /**
      * Creates a new instance.
      *
-     * @param engine A nashorn specific JavaScript engine with Lodash
-     *               pre-loaded into the global context as '_';
+     * @param engine A Nashorn specific JavaScript engine with Lodash
+     *               pre-loaded into the global context as '_'.
      */
     public LodashScriptImage(ScriptEngine engine) {
         super(engine);
@@ -39,5 +43,37 @@ public class LodashScriptImage extends AbstractScriptImage {
      */
     public String getUuid() {
         return (String) mirror.callMember("uuid");
+    }
+
+    /**
+     * Gets an array of parameters found in a JavaScript function.  For example,
+     * given the following script
+     *
+     * function render(foo, bar, baz) {
+     *     console.log(foo + bar + baz);
+     * }
+     *
+     * this method will return ["foo", "bar", "baz"].
+     *
+     * @param function A reference to a JavaScript function in Nashorn.
+     * @return A non-null String[], even if the JavaScript function has no
+     *         parameters.
+     */
+    public String[] getParameters(ScriptObjectMirror function) {
+        if (function == null) {
+            String msg = "The 'function' argument cannot be null.";
+            throw new IllegalArgumentException(msg);
+        }
+
+        String[] params;
+        try {
+            Object result = mirror.callMember("getParams", function);
+            ScriptObjectMirror array = (ScriptObjectMirror) result;
+            params = array.to(String[].class);
+        } catch (Exception ex) {
+            String msg = "The argument you supplied for 'function' is invalid.";
+            throw new IllegalArgumentException(msg);
+        }
+        return params;
     }
 }
--- src/test/java/io/redbarn/LodashScriptImageTest.java
+++ src/test/java/io/redbarn/LodashScriptImageTest.java
@@ -1,8 +1,12 @@
 package io.redbarn;
 
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import javax.script.ScriptException;
+
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
 /**
@@ -13,17 +17,41 @@ import static org.testng.Assert.assertNotNull;
  */
 public class LodashScriptImageTest extends AbstractScriptEngineTest {
 
-    @Test
+    @Test(groups = "Slow")
     public void constructor_EngineIsNotNull_MirrorIsSet() {
         LodashScriptImage lodash = new LodashScriptImage(scriptEngine);
         ScriptObjectMirror mirror = lodash.getMirror();
         assertNotNull(mirror);
     }
 
-    @Test
+    @Test(groups = "Slow")
     public void getUuid_Always_Succeeds() {
         LodashScriptImage lodash = new LodashScriptImage(scriptEngine);
         String uuid = lodash.getUuid();
         assertNotNull(uuid);
+    }
+
+    @Test(groups = "Slow", expectedExceptions = IllegalArgumentException.class)
+    public void getParameters_FunctionIsNull_Throws() {
+        LodashScriptImage lodash = new LodashScriptImage(scriptEngine);
+        lodash.getParameters(null);
+    }
+
+    @Test(groups = "Slow")
+    public void getParameters_FunctionIsValidWithNoParameters_ReturnsNonNull() throws ScriptException {
+        LodashScriptImage lodash = new LodashScriptImage(scriptEngine);
+        String definition = "function render() { }";
+        ScriptObjectMirror function = (ScriptObjectMirror) scriptEngine.eval(definition);
+        String[] params = lodash.getParameters(function);
+        assertNotNull(params);
+    }
+
+    @Test(groups = "Slow")
+    public void getParameters_FunctionIsValidWithParameters_ReturnsAllParameters() throws ScriptException {
+        LodashScriptImage lodash = new LodashScriptImage(scriptEngine);
+        String definition = "function render(foo, bar, baz) { }";
+        ScriptObjectMirror function = (ScriptObjectMirror) scriptEngine.eval(definition);
+        String[] params = lodash.getParameters(function);
+        assertEquals(params.length, 3);
     }
 }
